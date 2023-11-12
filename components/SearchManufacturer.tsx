30dbf0fed022b27fbc07edac98a35b18cefeba49
dodam24 <dodam0724@gmail.com>
2023-11-12 15:37:48 +0900

    자동차 제조사 검색 컴포넌트 설정

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -4,15 +4,17 @@ import { useState, Fragment } from "react";
 import Image from "next/image";
 import { Combobox, Transition } from "@headlessui/react";
 
-import { manufacturers } from "@/constants";
-import { SearchManufacturerProps } from "@/types";
+import { manufacturers } from "@/constants";  // 자동차 제조사 목록이 정의된 상수
+import { SearchManufacturerProps } from "@/types";  // SearchManufacturer 컴포넌트에 전달되는 프로퍼티 타입
 
+// SearchManufacturer 컴포넌트 정의
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState(""); // 검색 쿼리 상태
 
+  // 검색된 제조사 목록 필터링
   const filteredManufacturers =
     query === ""
       ? manufacturers
@@ -23,10 +25,12 @@ const SearchManufacturer = ({
             .includes(query.toLowerCase().replace(/\s+/g, ""))
         );
 
+  // 컴포넌트 렌더링
   return (
     <div className="search-manufacturer">
       <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
+          {/* Combobox 버튼. 아이콘을 클릭하면 전체 드롭다운이 표시된다. */}
           <Combobox.Button
             className="absolute top-
         [14px]"
@@ -40,21 +44,24 @@ const SearchManufacturer = ({
             />
           </Combobox.Button>
 
+          {/* 검색을 위한 입력 필드 */}
           <Combobox.Input
             className="search-manufacturer__input"
             placeholder="Volkswagen"
             displayValue={(manufacturer: string) => manufacturer}
-            onChange={(e) => setQuery(e.target.value)}
+            onChange={(e) => setQuery(e.target.value)}  // 입력이 변경될 때 검색 쿼리를 업데이트 한다.
           />
 
+          {/* 옵션을 표시하기 위한 Transition */}
           <Transition
-            as={Fragment}
+            as={Fragment} // <></>를 사용하여 추가 DOM 노드를 도입하지 않고 여러 요소를 그룹화 한다.
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
-            afterLeave={() => setQuery("")}
+            afterLeave={() => setQuery("")} // Transition 완료 후, 검색 쿼리를 재설정
           >
             <Combobox.Options>
+              {/* 필터링된 제조사 목록을 매핑하여 옵션을 생성 */}
               {filteredManufacturers.map((item) => (
                 <Combobox.Option
                   key={item}
@@ -66,10 +73,12 @@ const SearchManufacturer = ({
                 >
                   {({ selected, active }) => (
                     <>
+                      {/* 옵션이 선택된 경우, 글꼴을 진하게 설정 */}
                       <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
                         {item}
                       </span>
                       
+                      {/* 옵션이 선택된 경우, 활성 상태에 따라 파란색 배경을 표시 /*/}
                       {selected ? (
                         <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
                         ></span>
