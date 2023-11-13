dc1c3b6da67ccb8b04b9ea8f9b9d372471b54d6c
dodam24 <dodam0724@gmail.com>
2023-11-13 22:10:45 +0900

    검색 기능 구현

--- types/index.ts
+++ types/index.ts
@@ -1,21 +1,5 @@
 import { MouseEventHandler } from "react";
 
-export interface CustomButtonProps {
-  title: string;
-  containerStyles?: string;
-  handleClick?:
-  MouseEventHandler<HTMLButtonElement>;
-  btnType?: "button" | "submit";
-  textStyles?: string;
-  rightIcon?: string;
-  isDisabled?: boolean;
-}
-
-export interface SearchManufacturerProps {
-  manufacturer: string;
-  setManufacturer: (manufacturer: string) => void;
-}
-
 export interface CarProps {
   city_mpg: number;
   class: string;
@@ -29,4 +13,26 @@ export interface CarProps {
   model: string;
   transmission: string;
   year: number;
+}
+
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
+
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+
+export interface CarCardProps {
+  model: string;
+  make: string;
+  mpg: number;
+  transmission: string;
+  year: number;
+  drive: string;
+  cityMPG: number;
 }
