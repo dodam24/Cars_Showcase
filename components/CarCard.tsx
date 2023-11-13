c3c035936c2165ce67576211863204de703fd456
dodam24 <dodam0724@gmail.com>
2023-11-13 23:11:02 +0900

    고급 검색 기능 구현

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -43,37 +43,25 @@ const CarCard = ({ car }: CarCardProps) => {
       </div>
 
       <div className="relative flex w-full mt-2">
-        <div
-          className="flex group-hover:invisible
-        w-full justify-between text-gray"
-        >
-          <div
-            className="flex flex-col justify-center
-          items-center gap-2"
-          >
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
             <Image
               src="/steering-wheel.svg"
               width={20}
               height={20}
               alt="steering wheel"
             />
-            <p className="text-[14px]">
+            <p className="text-[14px] leading-[17px]">
               {transmission === "a" ? "Automatic" : "Manual"}
             </p>
           </div>
-          <div
-            className="flex flex-col justify-center
-          items-center gap-2"
-          >
+          <div className="car-card__icon">
             <Image src="/tire.svg" width={20} height={20} alt="tire" />
-            <p className="text-[14px]">{drive.toUpperCase()}</p>
+            <p className="car-card__icon-text">{drive.toUpperCase()}</p>
           </div>
-          <div
-            className="flex flex-col justify-center
-          items-center gap-2"
-          >
-            <Image src="/gas.svg" width={20} height={20} alt="steering wheel" />
-            <p className="text-[14px]">{city_mpg} MPG</p>
+          <div className="car-card__icon">
+            <Image src="/gas.svg" width={20} height={20} alt="gas" />
+            <p className="car-card__icon-text">{city_mpg} MPG</p>
           </div>
         </div>
 
--- types/index.ts
+++ types/index.ts
@@ -36,3 +36,33 @@ export interface CarCardProps {
   drive: string;
   cityMPG: number;
 }
+
+export interface CustomButtonProps {
+  isDisabled?: boolean;
+  btnType?: "button" | "submit";
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+}
+
+export interface SearchManuFacturerProps {
+  manufacturer: string;
+  setManuFacturer: (manufacturer: string) => void;
+}
